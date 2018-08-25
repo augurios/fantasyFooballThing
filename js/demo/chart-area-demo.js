f2f94f71a8fad064dcfcda0aa67271108b9eebb1
Augusto Valerio <auguvalerio@gmail.com>
2018-08-25 17:12:38 -0600

    new json requests

--- js/demo/chart-area-demo.js
+++ js/demo/chart-area-demo.js
@@ -2,55 +2,6 @@
 Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
 Chart.defaults.global.defaultFontColor = '#292b2c';
 
-// Area Chart Example
-var ctx = document.getElementById("myAreaChart");
-var myLineChart = new Chart(ctx, {
-  type: 'line', 
-  data: {
-    labels: ["2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017"],
-    datasets: [{
-      label: "Wins",
-      lineTension: 0.3,
-      backgroundColor: "rgba(2,117,216,0.2)",
-      borderColor: "rgba(2,117,216,1)",
-      pointRadius: 5,
-      pointBackgroundColor: "rgba(2,117,216,1)",
-      pointBorderColor: "rgba(255,255,255,0.8)",
-      pointHoverRadius: 5,
-      pointHoverBackgroundColor: "rgba(2,117,216,1)",
-      pointHitRadius: 50,
-      pointBorderWidth: 1,
-      data: [12, 8, 10, 12, 10, 13, 13, 13, 9],
-    }],
-  },
-  options: {
-    scales: {
-      xAxes: [{
-        time: {
-          unit: 'date'
-        },
-        gridLines: {
-          display: false
-        },
-        ticks: {
-          maxTicksLimit: 9
-        }
-      }],
-      yAxes: [{
-        gridLines: {
-          color: "rgba(0, 0, 0, .125)",
-        },
-		ticks: {
-			min: 0, // minimum value
-			max: 17, // maximum value
-			stepSize:1 
-		}
-      }],
-    },
-    legend: {
-      display: false
-    }
-  }
-});
+
 
 
